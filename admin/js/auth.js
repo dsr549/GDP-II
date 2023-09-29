a868462c0bf3035611a9a69f147ef436c11ba8e9
Bhanuteja009 <111711705+Bhanuteja009@users.noreply.github.com>
2023-09-29 09:04:08 -0500

    updated announcements and map pages

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,6 +1,6 @@
 if(!(sessionStorage.getItem('token'))){
     //location.href="../index.html";
-   // window.location.href="http://localhost:3000";
+    window.location.href="http://localhost:3000";
 } ///else {
    // window.location.href="http://localhost:3000";
   // }
